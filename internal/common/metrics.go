71f2dc7197f2382445b565b8e4a636d7d727de12
Kaushik Surya <kausyas@amazon.com>
2023-05-08 19:21:30 -0400

    increase sleep

--- internal/common/metrics.go
+++ internal/common/metrics.go
@@ -99,7 +99,7 @@ func SendCollectDMetrics(metricPerInterval int, sendingInterval, duration time.D
 		return err
 	}
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(30 * time.Second)
 
 	for {
 		select {
