ab01d13c98b5eb587807c6ea4e4d2a75b9a58dc5
Kaushik Surya <kausyas@amazon.com>
2023-06-13 11:30:50 -0400

    Run only one test

--- test/cloudwatchlogs/publish_logs_test.go
+++ test/cloudwatchlogs/publish_logs_test.go
@@ -44,12 +44,12 @@ var testParameters = []input{
 		numExpectedLogs: 200,
 		configPath:      "resources/config_log.json",
 	},
-	{
-		testName:        "Client-side log filtering",
-		iterations:      100,
-		numExpectedLogs: 100,
-		configPath:      "resources/config_log_filter.json",
-	},
+	//{
+	//	testName:        "Client-side log filtering",
+	//	iterations:      100,
+	//	numExpectedLogs: 100,
+	//	configPath:      "resources/config_log_filter.json",
+	//},
 }
 
 var envMetaDataStrings = &(environment.MetaDataStrings{})
@@ -119,70 +119,70 @@ func TestWriteLogsToCloudWatch(t *testing.T) {
 // 2. The file should be rotated, and a new log line of size N should be written
 // 3. The file should be rotated again, and a new log line of size GREATER THAN N should be written
 // 4. All three log lines, in full, should be visible in CloudWatch Logs
-func TestRotatingLogsDoesNotSkipLines(t *testing.T) {
-	cfgFilePath := "resources/config_log_rotated.json"
-
-	instanceId := awsservice.GetInstanceId()
-	log.Printf("Found instance id %s", instanceId)
-	logGroup := instanceId
-	logStream := instanceId + "Rotated"
-
-	defer awsservice.DeleteLogGroupAndStream(logGroup, logStream)
-
-	common.DeleteFile(common.AgentLogFile)
-	common.TouchFile(common.AgentLogFile)
-
-	start := time.Now()
-	common.CopyFile(cfgFilePath, configOutputPath)
-
-	common.StartAgent(configOutputPath, true, false)
-
-	// ensure that there is enough time from the "start" time and the first log line,
-	// so we don't miss it in the GetLogEvents call
-	time.Sleep(agentRuntime)
-	t.Log("Writing logs and rotating")
-	// execute the script used in the repro case
-	common.RunCommand("/usr/bin/python3 resources/write_and_rotate_logs.py")
-	time.Sleep(agentRuntime)
-	common.StopAgent()
-
-	agentLog, err := common.RunCommand(common.CatCommand + common.AgentLogFile)
-	if err != nil {
-		return
-	}
-	t.Logf("Agent logs %s", agentLog)
-
-	// These expected log lines are created using resources/write_and_rotate_logs.py,
-	// which are taken directly from the repro case in https://github.com/aws/amazon-cloudwatch-agent/issues/447
-	// logging.info(json.dumps({"Metric": "12345"*10}))
-	// logging.info(json.dumps({"Metric": "09876"*10}))
-	// logging.info({"Metric": "1234567890"*10})
-	lines := []string{
-		fmt.Sprintf("{\"Metric\": \"%s\"}", strings.Repeat("12345", 10)),
-		fmt.Sprintf("{\"Metric\": \"%s\"}", strings.Repeat("09876", 10)),
-		fmt.Sprintf("{\"Metric\": \"%s\"}", strings.Repeat("1234567890", 10)),
-	}
-
-	end := time.Now()
-
-	ok, err := awsservice.ValidateLogs(logGroup, logStream, &start, &end, func(logs []string) bool {
-		if len(logs) != len(lines) {
-			return false
-		}
-
-		for i := 0; i < len(logs); i++ {
-			expected := strings.ReplaceAll(lines[i], "'", "\"")
-			actual := strings.ReplaceAll(logs[i], "'", "\"")
-			if expected != actual {
-				return false
-			}
-		}
-
-		return true
-	})
-	assert.NoError(t, err)
-	assert.True(t, ok)
-}
+//func TestRotatingLogsDoesNotSkipLines(t *testing.T) {
+//	cfgFilePath := "resources/config_log_rotated.json"
+//
+//	instanceId := awsservice.GetInstanceId()
+//	log.Printf("Found instance id %s", instanceId)
+//	logGroup := instanceId
+//	logStream := instanceId + "Rotated"
+//
+//	defer awsservice.DeleteLogGroupAndStream(logGroup, logStream)
+//
+//	common.DeleteFile(common.AgentLogFile)
+//	common.TouchFile(common.AgentLogFile)
+//
+//	start := time.Now()
+//	common.CopyFile(cfgFilePath, configOutputPath)
+//
+//	common.StartAgent(configOutputPath, true, false)
+//
+//	// ensure that there is enough time from the "start" time and the first log line,
+//	// so we don't miss it in the GetLogEvents call
+//	time.Sleep(agentRuntime)
+//	t.Log("Writing logs and rotating")
+//	// execute the script used in the repro case
+//	common.RunCommand("/usr/bin/python3 resources/write_and_rotate_logs.py")
+//	time.Sleep(agentRuntime)
+//	common.StopAgent()
+//
+//	agentLog, err := common.RunCommand(common.CatCommand + common.AgentLogFile)
+//	if err != nil {
+//		return
+//	}
+//	t.Logf("Agent logs %s", agentLog)
+//
+//	// These expected log lines are created using resources/write_and_rotate_logs.py,
+//	// which are taken directly from the repro case in https://github.com/aws/amazon-cloudwatch-agent/issues/447
+//	// logging.info(json.dumps({"Metric": "12345"*10}))
+//	// logging.info(json.dumps({"Metric": "09876"*10}))
+//	// logging.info({"Metric": "1234567890"*10})
+//	lines := []string{
+//		fmt.Sprintf("{\"Metric\": \"%s\"}", strings.Repeat("12345", 10)),
+//		fmt.Sprintf("{\"Metric\": \"%s\"}", strings.Repeat("09876", 10)),
+//		fmt.Sprintf("{\"Metric\": \"%s\"}", strings.Repeat("1234567890", 10)),
+//	}
+//
+//	end := time.Now()
+//
+//	ok, err := awsservice.ValidateLogs(logGroup, logStream, &start, &end, func(logs []string) bool {
+//		if len(logs) != len(lines) {
+//			return false
+//		}
+//
+//		for i := 0; i < len(logs); i++ {
+//			expected := strings.ReplaceAll(lines[i], "'", "\"")
+//			actual := strings.ReplaceAll(logs[i], "'", "\"")
+//			if expected != actual {
+//				return false
+//			}
+//		}
+//
+//		return true
+//	})
+//	assert.NoError(t, err)
+//	assert.True(t, ok)
+//}
 
 func writeLogs(t *testing.T, f *os.File, iterations int) {
 	log.Printf("Writing %d lines to %s", iterations*len(logLineIds), f.Name())
